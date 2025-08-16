bf5e85ba3effa8c956d92b9b8a94db2cbcdac5fb
yagen <yagen1111@gmail.com>
2025-08-16 21:45:19 +0300

    Restructure repository: move infrastructure modules to root level

--- infrastructure-modules/argocd/main.tf
+++
@@ -1,8 +0,0 @@
-resource "helm_release" "argocd" {
-  name       = "argocd"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  namespace  = "argocd"
-  
-  create_namespace = true
-}
